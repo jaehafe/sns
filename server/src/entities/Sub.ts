81fd5dd1195e72a72d84308e1a98e22219774748
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-03 21:33:11 +0900

    Feat: Sub 엔티티 생성

---
+++ server/src/entities/Sub.ts
@@ -0,0 +1,59 @@
+import { Expose } from 'class-transformer';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { User } from '../entity/User';
+
+@Entity('subs')
+export default class Sub extends BaseEntity {
+  @Index()
+  @Column({ unique: true })
+  name: string;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ nullable: true })
+  imageUrn: string;
+
+  @Column({ nullable: true })
+  bannerUrn: string;
+
+  @Column()
+  username: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'username', referencedColumnName: 'username' })
+  user: User;
+
+  @OneToMany(() => postMessage, (post) => post.sub)
+  posts: Post[];
+
+  @Expose()
+  get imageUrl(): string {
+    return this.imageUrn
+      ? `${process.env.APP_URL}/images/${this.imageUrn}`
+      : 'https://www.gravatar.com/avatar?d=m&f=y';
+  }
+
+  @Expose()
+  get bannerUrl(): string | undefined {
+    return this.bannerUrn ? `${process.env.APP_URL}/images/${this.bannerUrn}` : undefined;
+  }
+}
+
+/**
+ * @JoinColumn
+ * name 속성은 현재 엔티티(Sub)에서 사용될 외래키 컬럼의 이름을 지정.
+ * referencedColumnName 속성은 연결되는 엔티티(User)에서 사용될 컬럼의 이름을 지정.
+ */
--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,18 +1,48 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsEmail, Length } from 'class-validator';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Index,
+  OneToMany,
+  BeforeInsert,
+  BaseEntity,
+} from 'typeorm';
+import bcrypt from 'bcryptjs';
+import { Exclude } from 'class-transformer';
 
-@Entity()
-export class User {
+@Entity('users')
+export default class User extends BaseEntity {
+  @Index()
+  @IsEmail(undefined, { message: '이메일 주소가 잘못되었습니다.' })
+  @Length(1, 255, { message: '이메일 주소는 비워둘 수 없습니다.' })
+  @Column({ unique: true })
+  email: string;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Index()
+  @Length(3, 32, { message: '사용자 이름은 3자 이상이어야 합니다.' })
+  @Column()
+  username: string;
 
-    @Column()
-    firstName: string
+  @Exclude()
+  @Column()
+  @Length(6, 255, { message: '비밀번호는 6자리 이상이어야 합니다.' })
+  password: string;
 
-    @Column()
-    lastName: string
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 
-    @Column()
-    age: number
+  @OneToMany(() => Vote, (vote) => vote.user)
+  votes: Vote[];
 
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
 }
+
+/**
+ * @OneToMany
+ * typeFunction은 매핑할 다른 엔티티의 클래스 타입을 지정하는 함
+ * inverseSideProperty는 연결된 다른 엔티티에서 현재 엔티티와 관련된 역방향 참조를 나타내는 속성 이름을 지정
+ */

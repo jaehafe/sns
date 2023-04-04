26242e98745cfb62d406b6a68d3cd9552fbc67fd
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-04 20:34:15 +0900

    Feat: [client] sub create 페이지 추가

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -58,19 +58,19 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     defaultDispatch({ type, payload });
   };
 
-  useEffect(() => {
-    async function loadUser() {
-      try {
-        const res = await axios.get('/auth/me');
-        dispatch('LOGIN', res.data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        dispatch('STOP_LOADING');
-      }
-    }
-    loadUser();
-  }, []);
+  // useEffect(() => {
+  //   async function loadUser() {
+  //     try {
+  //       const res = await axios.get('/auth/me');
+  //       dispatch('LOGIN', res.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     } finally {
+  //       dispatch('STOP_LOADING');
+  //     }
+  //   }
+  //   loadUser();
+  // }, []);
 
   return (
     <DispatchContext.Provider value={dispatch}>

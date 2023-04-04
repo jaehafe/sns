b5b5617139890c9cdb7c4167d68ef93a3743fbe0
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-04 20:34:50 +0900

    Feat: [server] 유저/로그인 인증 미들웨어 추가, subs router 추가

---
+++ server/src/middlewares/user.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../entities/User';
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // 요청에 있는 쿠키의 토큰 가져오기
+    const token = req.cookies.token;
+    if (!token) return next();
+
+    // verify메서드, jwt secret을 이용해서 토큰을 decode
+    const { username }: any = jwt.verify(token, process.env.JWT_SECRET!);
+
+    // 토큰에서 나온 유저 이름을 가지고 db에서 유저 정보 가져오기
+    const user = await User.findOneBy({ username });
+
+    // 유저 정보를 res.local.user에 넣어주기
+    res.locals.user = user;
+
+    return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ error: 'Something went wrong' });
+  }
+};
---
+++ server/src/routes/subs.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import userMiddleware from '../middlewares/user';
+import authMiddleware from '../middlewares/auth';
+
+const createSub = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, title, description } = req.body;
+};
+
+const router = Router();
+
+router.post('/', userMiddleware, authMiddleware, createSub);
+
+export default router;

cbf370081c98e42273ec063ec3728680ceb0a7ff
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-05 01:16:21 +0900

    Feat: [client] Subs data 가져오기, nextjs Image config 등록

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,8 +6,10 @@ import Link from 'next/link';
 import axios from 'axios';
 import useSWR from 'swr';
 import { Sub } from '../types';
+import { useAuthState } from '../context/auth';
 
 const Home: NextPage = () => {
+  const { authenticated } = useAuthState();
   const fetcher = async (url: string) => {
     return await axios.get(url).then((res) => res.data);
   };
@@ -29,15 +31,40 @@ const Home: NextPage = () => {
           </div>
 
           {/* 커뮤니티 리스트 */}
-          <div></div>
-
-          <div className="w-full py-6 text-center">
-            <Link legacyBehavior href="/subs/create">
-              <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
-                커뮤니티 만들기
-              </a>
-            </Link>
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link legacyBehavior href={`/r/${sub.name}`}>
+                  <a>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                </Link>
+                <Link legacyBehavior href={`/r/${sub.name}`}>
+                  <a className="ml-2 font-bold hover:cursor-pointer">/r/{sub.name}</a>
+                </Link>
+                <p className="ml-auto font-md">{sub.postCount}</p>
+              </div>
+            ))}
           </div>
+
+          {authenticated && (
+            <div className="w-full py-6 text-center">
+              <Link legacyBehavior href="/subs/create">
+                <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
+                  커뮤니티 만들기
+                </a>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>

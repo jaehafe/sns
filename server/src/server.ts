b01626659b8d20da1566643f49d2c61a69215a4f
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-04 20:41:33 +0900

    Feat: cookie-parser 라이브러리 추가 및 server.ts에 등록

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth';
 import subRoutes from './routes/subs';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const origin = 'http://localhost:3000';
@@ -12,6 +13,7 @@ const origin = 'http://localhost:3000';
 app.use(cors({ origin, credentials: true }));
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 dotenv.config();
 

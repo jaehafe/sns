810b00e55b4ae0100a9c761046f5758603f449b1
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-05 18:41:24 +0900

    Feat: [server] 게시물 댓글 작성하기 로직 추가

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -3,6 +3,7 @@ import userMiddleware from '../middlewares/user';
 import authMiddleware from '../middlewares/auth';
 import Sub from '../entities/Sub';
 import Post from '../entities/Post';
+import Comment from '../entities/Comment';
 
 const router = Router();
 
@@ -54,7 +55,31 @@ const getPost = async (req: Request, res: Response) => {
   }
 };
 
+const createPostComment = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  const body = req.body.body; // body: newComment,
+
+  try {
+    const post = await Post.findOneByOrFail({ identifier, slug });
+    const comment = new Comment();
+    comment.body = body;
+    comment.user = res.locals.user;
+    comment.post = post;
+
+    if (res.locals.user) {
+      post.setUserVote(res.locals.user);
+    }
+
+    await comment.save();
+    return res.json(comment);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ error: '게시물을 찾을 수 없습니다.' });
+  }
+};
+
 router.get('/:identifier/:slug', userMiddleware, getPost);
 router.post('/', userMiddleware, authMiddleware, createPost);
+router.post('/:identifier/:slug/comments', userMiddleware, createPostComment);
 
 export default router;

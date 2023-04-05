f9cbe6cb2ff406bee0c9bc2c058b19b182098110
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-05 18:06:45 +0900

    Feat: [client] font-awesome cdn 추가

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import NavBar from '../components/NavBar';
 import { SWRConfig } from 'swr';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api`;
@@ -24,14 +25,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   };
 
   return (
-    <SWRConfig value={{ fetcher }}>
-      <AuthProvider>
-        {!authRoute && <NavBar />}
-        <div className={authRoute ? '' : 'pt-12 bg-gray-200 min-h-screen'}>
-          <Component {...pageProps} />
-        </div>
-      </AuthProvider>
-    </SWRConfig>
+    <>
+      <Head>
+        <script
+          defer
+          src="https://use.fontawesome.com/releases/v6.1.1/js/all.js"
+          integrity="sha384-xBXmu0dk1bEoiwd71wOonQLyH+VpgR1XcDH3rtxrLww5ajNTuMvBdL5SOiFZnNdp"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <SWRConfig value={{ fetcher }}>
+        <AuthProvider>
+          {!authRoute && <NavBar />}
+          <div className={authRoute ? '' : 'pt-12 bg-gray-200 min-h-screen'}>
+            <Component {...pageProps} />
+          </div>
+        </AuthProvider>
+      </SWRConfig>
+    </>
   );
 }
 

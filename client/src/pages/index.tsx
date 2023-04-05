532161059d267de607cbb12782b5f6afff75f402
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-06 00:51:37 +0900

    Feat: [client] 무한스크롤 기능 추가

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,8 +7,12 @@ import { Post, Sub } from '../types';
 import { useAuthState } from '../context/auth';
 import useSWRInfinite from 'swr/infinite';
 import axios from 'axios';
+import PostCard from '../components/PostCard';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
+  const [observedPost, setObservedPost] = useState('');
+
   const { authenticated } = useAuthState();
 
   // const fetcher = async (url: string) => {
@@ -31,12 +35,49 @@ const Home: NextPage = () => {
     isValidating,
     mutate,
   } = useSWRInfinite<Post[]>(getKey);
-  console.log('data>>', data);
+  const isInitialLoading = !data && !error;
+  const posts: Post[] = data ? ([] as Post[]).concat(...data) : [];
+
+  useEffect(() => {
+    if (!posts || posts.length === 0) return;
+    // posts 배열의 마지막 post id를 가져오기
+    const id = posts[posts.length - 1].identifier;
+
+    // posts에 새로운 post가 추가되어 마지막의 post가 변경됐다면, 바뀐 post 중 마지막 post를 observedPost로 할당
+    if (id !== observedPost) {
+      setObservedPost(id);
+      observeElement(document.getElementById(id) as HTMLElement);
+    }
+  }, [posts]);
+
+  const observeElement = (element: HTMLElement | null) => {
+    if (!element) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        console.log('entries>>', entries);
+        if (entries[0].isIntersecting === true) {
+          console.log('현재 화면에서 마지막 post 입니다.');
+          setPage(page + 1);
+          // 관찰 해제
+          observer.unobserve(element);
+        }
+      },
+      { threshold: 1 }
+    );
+    // 새로 관찰
+    observer.observe(element);
+  };
 
   return (
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
       {/* post list */}
-      <div className="w-full md:mr-3 md:w-8/12"></div>
+      <div className="w-full md:mr-3 md:w-8/12">
+        {isInitialLoading && <p className="text-lg text-center">로딩중입니다...</p>}
+        {posts?.map((post) => (
+          <PostCard key={post.identifier} post={post} mutate={mutate} />
+        ))}
+      </div>
 
       {/* sidebar */}
       <div className="hidden w-4/12 ml-3 md:block">

8408d84f1c9554cc49b0d76fb0a0f9a6ea943892
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-05 17:48:31 +0900

    Feat: [server] 게시물 가져오기 로직 추가

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -31,6 +31,30 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
+const getPost = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+
+  try {
+    const post = await Post.findOneOrFail({
+      where: {
+        identifier,
+        slug,
+      },
+      relations: ['sub', 'votes'],
+    });
+
+    if (res.locals.user) {
+      post.setUserVote(res.locals.user);
+    }
+
+    return res.send(post);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ error: '게시물을 찾을 수 없습니다.' });
+  }
+};
+
+router.get('/:identifier/:slug', userMiddleware, getPost);
 router.post('/', userMiddleware, authMiddleware, createPost);
 
 export default router;
